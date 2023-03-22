8f17e97cae6b8558790250f957821a1aef6b97c6
ftsuda-senac <ftsuda.senac@gmail.com>
2023-03-21 22:36:56 -0300

    Projeto com exemplos Javascript

---
+++ exemplos-js/src/main/java/br/senac/tads/dsw/exemplosjs/WebMvcConfig.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.senac.tads.dsw.exemplosjs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("*")
+				.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+				.allowedHeaders("*")
+				.maxAge(3600); // 1 hora
+	}
+
+}
---
+++ exemplos-js/src/test/java/br/senac/tads/dsw/exemplosjs/ExemplosJsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senac.tads.dsw.exemplosjs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemplosJsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
