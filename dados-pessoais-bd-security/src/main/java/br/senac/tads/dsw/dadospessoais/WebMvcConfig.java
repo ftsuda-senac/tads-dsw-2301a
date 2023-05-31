5b4469a7af15f3b49fc96f88f5c1944f61ada3c7
ftsuda-senac <ftsuda.senac@gmail.com>
2023-05-30 22:23:03 -0300

    Ajuste para liberar CORS

---
+++ dados-pessoais-bd-security/src/main/java/br/senac/tads/dsw/dadospessoais/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package br.senac.tads.dsw.dadospessoais;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                .allowedHeaders("*")
+                .maxAge(3600); // 1 hora
+    }
+
+}
